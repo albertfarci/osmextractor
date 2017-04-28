81b73a19d688a81de1aaf3babe0eaaeba4378395
Alberto Farci <alberto@tarci.local>
2017-04-28 17:50:36 +0200

    no message

---
+++ OsmDBpedia/osmDbpedia.js
@@ -0,0 +1,41 @@
+var fs = require("fs");
+
+
+var overpass = fs.readFileSync("../Overpass/overpass.json");
+var jsonOverpass = JSON.parse(overpass);
+
+var dbpedia = fs.readFileSync("../DBpedia/dbpedia.jsonld");
+var jsonDbpedia = JSON.parse(dbpedia);
+
+merge();
+
+function merge(){
+
+	let osmDbpedia=[];
+
+	for (var itemOverpass of jsonOverpass){
+
+		for (var itemDBpedia of jsonDbpedia["@graph"]){
+
+			if(itemOverpass.tags.wikidata == itemDBpedia["http://www.w3.org/2002/07/owl#sameAs"]["@id"].split("http://www.wikidata.org/entity/")[1]){
+				
+				itemOverpass.dbpedia=itemDBpedia["@id"];
+
+				osmDbpedia.push(itemOverpass);
+			}
+
+		}
+
+	}
+
+	writeRdf(JSON.stringify(osmDbpedia));
+
+}
+
+function writeRdf(geojson){
+  fs.writeFile('./osmDbpedia.json', geojson, function (err) {
+    if (err)
+      return console.log(err);
+
+  });
+}
---
+++ Wikidata/wikidataExtractor.js
@@ -0,0 +1,88 @@
+var fs = require("fs");
+var request = require('request');
+ 
+var osmDbpedia = fs.readFileSync("../OsmDBpedia/osmDbpedia.json");
+var jsonOsmDbpedia = JSON.parse(osmDbpedia);
+ 
+createWikidataJson(jsonOsmDbpedia).then((arrayWikiData) => {
+
+	writeRdf(JSON.stringify(arrayWikiData));
+
+
+});
+
+
+function writeRdf(geojson){
+  fs.writeFile('./OsmDBpediaWikidata.jsonld', geojson, function (err) {
+    if (err)
+      return console.log(err);
+
+  });
+
+}
+
+function addItem(item, i){
+	return new Promise((resolve,reject) => {
+		let query=encodeURIComponent(`
+			PREFIX wikibase: <http://wikiba.se/ontology#> 
+			PREFIX wd: <http://www.wikidata.org/entity/> 
+			SELECT * 
+			WHERE{ 
+					BIND(wd:${item.tags.wikidata} AS ?wikidata)?sitelink schema:about ?wikidata. 
+				FILTER REGEX(STR(?sitelink), ".wikipedia.org/wiki/") . 
+			}`);
+
+		console.log(query);
+
+		var options = {
+		  url: 'https://query.wikidata.org/sparql?query='+query,
+		  headers: {
+		    'Accept': 'application/json'
+		  }
+		};
+		setTimeout(function() {
+		request(options, function (error, response, body) {
+	      
+			if(error){
+		        return reject(error);
+		      }
+
+		      let wikiObject={};
+
+		      
+		      body=JSON.parse(body);
+		      console.log(body);
+
+		     for (var wiki of body.results.bindings){
+
+		     	  let lang=wiki.sitelink.value.split("https://")[1].split(".")[0];
+
+		     	  wikiObject[lang]=wiki.sitelink.value;
+
+		     }
+
+		     item.wikipediaLinks=wikiObject;
+
+	      	
+		     return resolve(item);
+		    
+
+	    });
+	    }, 1000*i);
+
+	});
+}
+
+function createWikidataJson(geoJson){
+
+    let promises=[];
+    let count = 0;
+    for(var item of geoJson){
+    	count++;
+        promises.push(addItem(item, count ));
+
+    }
+
+    return Promise.all(promises);
+
+}
